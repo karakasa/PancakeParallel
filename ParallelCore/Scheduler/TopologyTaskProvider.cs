5cd22459303365046add48d24e694afd82d1a54b
Gan Keyu <gankeyu@hotmail.com>
2021-03-12 19:27:41 +0800

    Added topology task provider

--- ParallelCore/Scheduler/TopologyTaskProvider.cs
+++ ParallelCore/Scheduler/TopologyTaskProvider.cs
@@ -9,24 +9,129 @@ namespace ParallelCore.Scheduler
 {
     public class TopologyTaskProvider : ITaskProvider<int>
     {
+        public TopologyTaskProvider(ICollection<ITopologyNode> nodes)
+        {
+            Initialize(nodes);
+        }
         public void MarkAsCompleted(int identifier)
         {
-            throw new NotImplementedException();
+            _nodeStatus[identifier] = TaskStatus.Completed;
         }
 
         public void MarkAsRunning(int identifier)
         {
-            throw new NotImplementedException();
+            _nodeStatus[identifier] = TaskStatus.Running;
         }
 
         public void RunTaskSynchronously(int identifier)
         {
-            throw new NotImplementedException();
+            _indexedNodes[identifier].Node.Compute();
         }
 
         public TaskProviderState TryGetNextAvailableTask(out int identifier)
         {
-            throw new NotImplementedException();
+            var hasRuning = false;
+
+            for (var i = 0; i < _indexedNodes.Length; i++)
+            {
+                switch (_nodeStatus[i])
+                {
+                    case TaskStatus.Pending:
+                        identifier = i;
+                        return TaskProviderState.Normal;
+                    case TaskStatus.Running:
+                        hasRuning = true;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            identifier = -1;
+            return hasRuning ? TaskProviderState.NoAvailableTask : TaskProviderState.AllComplete;
+        }
+        private struct AbstractNode
+        {
+            public ITopologyNode Node;
+            public List<ITopologyNode> Sources;
+        }
+
+        private struct CachedNode
+        {
+            public ITopologyNode Node;
+            public int[] SourceIds;
+        }
+        private List<AbstractNode> TopologicalSort(List<AbstractNode> nodes)
+        {
+            var list = new List<AbstractNode>(nodes.Count);
+
+            for (; ; )
+            {
+                var found = false;
+
+                for (var i = 0; i < nodes.Count; i++)
+                {
+                    var it = nodes[i];
+
+                    if (it.Sources.Count == 0)
+                    {
+                        list.Add(it);
+
+                        for (var j = 0; j < nodes.Count; j++)
+                        {
+                            if (i == j)
+                                continue;
+
+                            nodes[j].Sources.RemoveAll(src => object.ReferenceEquals(src, it.Node));
+                        }
+
+                        nodes.RemoveAt(i);
+                        found = true;
+
+                        break;
+                    }
+                }
+
+                if (nodes.Count == 0)
+                    break;
+
+                if (!found)
+                {
+                    // Loop in the graph
+                    throw new InvalidOperationException("Loop detected in the topological graph.");
+                }
+            }
+
+            return list;
+        }
+
+        private CachedNode[] ToIndexedNodes(List<AbstractNode> nodes)
+        {
+            var dict = new Dictionary<ITopologyNode, int>();
+
+            for (var i = 0; i < nodes.Count; i++)
+                dict[nodes[i].Node] = i;
+
+            return nodes.Select(node => new CachedNode
+            {
+                Node = node.Node,
+                SourceIds = node.Sources.Select(src => dict[src]).ToArray()
+            }).ToArray();
+        }
+        private CachedNode[] _indexedNodes;
+        private TaskStatus[] _nodeStatus;
+        private int _nextTask = 0;
+        private void Initialize(ICollection<ITopologyNode> nodes)
+        {
+            var _nodes = nodes.Select(n => new AbstractNode
+            {
+                Node = n,
+                Sources = n.Sources.ToList()
+            }).ToList();
+
+            _nodes = TopologicalSort(_nodes);
+            _indexedNodes = ToIndexedNodes(_nodes);
+            _nodeStatus = new TaskStatus[_indexedNodes.Length];
         }
     }
 }
